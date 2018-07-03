95d32ba3a95c9d7d875a8792a7d4d16f95a3f22d
Polands in the Sky <glugglugglug@vivaldi.net>
2018-07-03 09:21:18 +0800

    blablab;la

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+import praw
+import pdb
+import re
+import os
 import discord
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import time
+import json
